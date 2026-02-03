6c9a5ec087ccab8ee880b28cf1e04004bf906488
MuhammadArifinDava <adava05@gmail.com>
2026-02-03 09:04:44 +0800

    chore: add socket.io-client import to CrosswordPlayer
    The import is needed for future real-time multiplayer functionality.

--- CrosswordApp/client/src/pages/CrosswordPlayer.jsx
+++ CrosswordApp/client/src/pages/CrosswordPlayer.jsx
@@ -14,6 +14,7 @@ import { useTheme } from "../hooks/useTheme";
 import { useAchievements } from "../hooks/useAchievements";
 import { useDragScroll } from "../hooks/useDragScroll";
 import { useAuth } from "../context/useAuth";
+import { io } from "socket.io-client";
 
 const ShareModal = lazy(() => import("../components/ShareModal"));
 
